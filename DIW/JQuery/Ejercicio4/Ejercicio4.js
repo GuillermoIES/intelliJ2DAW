952f93512409a4217a7f12213617edee1c1c5a38
Hector <hector.zaragoza.arranz@gmail.com>
2017-01-25 12:34:07 +0100

    25/01/17 12:34

--- DIW/JQuery/Ejercicio4/Ejercicio4.js
+++ DIW/JQuery/Ejercicio4/Ejercicio4.js
@@ -8,36 +8,40 @@ $(document).ready(function () {
 })
 function movimiento(e) {
     var code = e.which;
-        if (code == 37) {
-            var l = parseInt($d.css('left')) - vel;
-            $d.css('left', l);
-            $d.css('background-position', '0 0');
-            clase = 'izquierda';
-        }
-        else if (code == 38) {
-            var t = parseInt($d.css('top')) - vel;
-            $d.css('top', t);
-            $d.css('background-position', '0 -192px');
-            clase = 'arriba';
-        }
-        else if (code == 39) {
-            var l = parseInt($d.css('left')) + vel;
-            $d.css('left', l);
-            $d.css('background-position', '0 -96px');
-            clase = 'derecha';
-        }
-        else if (code == 40) {
-            var t = parseInt($d.css('top')) + vel;
-            $d.css('top', t);
-            $d.css('background-position', '0 -288px');
-            clase = 'abajo';
-        }
-          $d.addClass(clase);
+    if (code == 37) {
+        var l = (parseInt($d.css('left')) - vel);
+        $d.css('left', l);
+        $d.css('background-position', '0 0');
+        clase = 'izquierda';
+    }
+    else if (code == 38) {
+        var t = parseInt($d.css('top')) - vel;
+        $d.css('top', t);
+        $d.css('background-position', '0 -192px');
+        clase = 'arriba';
+    }
+    else if (code == 39) {
+        var l = parseInt($d.css('left')) + vel;
+        $d.css('left', l);
+        $d.css('background-position', '0 -96px');
+        clase = 'derecha';
+    }
+    else if (code == 40) {
+        var t = parseInt($d.css('top')) + vel;
+        $d.css('top', t);
+        $d.css('background-position', '0 -288px');
+        clase = 'abajo';
+    }
+    if (parseInt($d.css('left')) < 50) $d.css('left', $(window).width()-100);
+    if (parseInt($d.css('left')) > $(window).width()-100) $d.css('left', 50);
+    if (parseInt($d.css('top')) < 50) $d.css('top', $(window).height()-110);
+    if (parseInt($d.css('top')) > $(window).height()-110) $d.css('top', 50);
+    $d.addClass(clase);
 }
-function parar(){
-   $d.removeClass('arriba');
+function parar() {
+    $d.removeClass('arriba');
     $d.removeClass('abajo');
     $d.removeClass('derecha');
     $d.removeClass('izquierda');
-   clase = null;
+    clase = null;
 }
