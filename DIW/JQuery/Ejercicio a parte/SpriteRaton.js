265b208fd56bf4a2ee4fa25d57eb6712c9ee6b07
Hector <hector.zaragoza.arranz@gmail.com>
2017-02-09 14:21:05 +0100

    juego currao

--- DIW/JQuery/Ejercicio a parte/SpriteRaton.js
+++ DIW/JQuery/Ejercicio a parte/SpriteRaton.js
@@ -1,45 +1,155 @@
 var $d;
-var vel = 5;
-$(document).ready(function (){
+var $esp;
+var $body;
+var vel = 1;
+var inter;
+var puntos = 0;
+var pierdes = false;
+$(document).ready(function () {
     $d = $('#mover');
-    $('body').css('width', $(window).width());
-    $('body').css('height', $(window).height());
-    $('body').on('mousemove', movimiento);
+    $esp = $('#espada');
+    $body = $('body');
+    $body.on('mousemove', actualizaCoord);
+    setTimeout(function(){
+    inter = setInterval('movimiento()', 1);
+    setInterval('genMoneda()', 1000);
+    $('#mover').on('click', auch);
+    }, 1000);
+    $('#puntos').text(puntos);
+    setInterval('perder()', 50);
 });
-function movimiento(e) {
-        var derecha;
-        var abajo;
-        var xRaton = e.pageX;
-        var yRaton = e.pageY;
-        var xLink = parseInt($d.css('left'));
-        var yLink = parseInt($d.css('top'));
-        if (xRaton > xLink) {
-            derecha = true;
-        } else {
+function perder(){
+    //TODO comprobar si a perdido y decir que pierdes y mostrar puntuacion
+}
+function genMoneda(){
+        var x = Math.floor(Math.random() * parseInt($body.css('width')));
+        var y = Math.floor(Math.random() * parseInt($body.css('height')));
+        var $moneda = $('<div></div>');
+        $moneda.addClass('moneda');
+        $moneda.css('top', y);
+        $moneda.css('left', x);
+        $body.append($moneda);
+        $moneda.on('mouseenter', ganaPunto);
+        if($('div.moneda').length > 100) pierdes = true;
+}
+function ganaPunto(e){
+    if(!cogido) {
+        puntos += 1;
+        e.target.remove();
+        $('#puntos').text(puntos);
+    }
+}
+var ejecutando = false;
+var seguir = true;
+var xRaton;
+var yRaton;
+var cogido = false;
+function auch(e) {
+    if(!ejecutando) {
+        ejecutando = true;
+        var $a = $('#auch');
+        var x = parseInt($d.css('left')) + parseInt($d.css('width')) + 10;
+        var y = parseInt($d.css('top')) + parseInt($d.css('height')) / 3;
+        $a.css('left', x);
+        $a.css('top', y);
+        $a.css('display', 'block');
+        var ouch = new Audio('ouch.mp3');
+        ouch.play();
+        clearInterval(inter);
+        cogido = false;
+        seguir = true;
+        $esp.css('left', parseInt(e.pageX) - 5);
+        $esp.css('top', parseInt(e.pageY) - parseInt($esp.css('height')));
+        setTimeout(function () {
+            inter = setInterval('movimiento()', 1);
+            ejecutando = false;
+        }, 1000);
+        setTimeout(function () {
+            $a.css('display', 'none');
+        }, 200);
+        $esp.css('transform', 'rotate(90deg)');
+        setTimeout(function () {
+            $esp.css('transform', 'rotate(0deg)');
+        }, 500);
+    }
+}
+function actualizaCoord(e) {
+    xRaton = e.pageX - parseInt($d.css('width')) / 2;
+    yRaton = e.pageY - parseInt($d.css('height'));
+
+    if (!cogido) {
+        $esp.css('left', parseInt(e.pageX) - 5);
+        $esp.css('top', parseInt(e.pageY) - parseInt($esp.css('height')));
+    }
+
+}
+function movimiento() {
+    var derecha;
+    var abajo;
+    var xLink = parseInt($d.css('left'));
+    var yLink = parseInt($d.css('top'));
+    if (xRaton > xLink) {
+        if(seguir) derecha = true;
+        else
             derecha = false;
-        }
-        if (yRaton > yLink) {
-            abajo = true;
-        } else {
+    } else if (xRaton < xLink) {
+        if(seguir) derecha = false;
+        else
+            derecha = true;
+    } else {
+        derecha = null;
+    }
+    if (yRaton > yLink) {
+        if(seguir) abajo = true;
+        else
             abajo = false;
-        }
-        var l = (derecha ? xLink + vel : xLink - vel);
-        var t = (abajo ? yLink + vel : yLink - vel);
-        borrarTodo();
-        if (derecha) {
-            borrarTodo()
-            $d.addClass('derecha');
-        } else {
-            $d.addClass('izquierda');
-        }
+    } else if (yRaton < yLink) {
+        if(seguir)
+            abajo = false;
+        else
+            abajo = true;
+    } else {
+        abajo = null;
+    }
+    var l = xLink;
+    if (derecha != null)
+        l = (derecha ? xLink + vel : xLink - vel);
+    var t = yLink;
+    if (abajo != null)
+        t = (abajo ? yLink + vel : yLink - vel);
+
+    if(!seguir){
+        t = (t<0?0:t);
+        t = (t>$body.height()-70?$body.height()-70:t);
+        l = (l<0?0:l);
+        l = (l>$body.width()?$body.width():l);
+    }
+    borrarTodo();
+    if (abajo != null) {
         if (abajo) {
             $d.addClass('abajo');
         } else {
             $d.addClass('arriba');
         }
-        $d.css('left', l);
-        $d.css('top', t);
-        movimiento;
+    }
+    if (derecha != null) {
+        if (derecha) {
+            $d.addClass('derecha');
+        } else {
+            $d.addClass('izquierda');
+        }
+    }
+    if (!cogido && derecha == null && abajo == null) {
+        cogido = true;
+        seguir = false;
+        new Audio('heylisten.mp3').play();
+    }
+    $d.css('left', l);
+    $d.css('top', t);
+    if (cogido) {
+        $esp.css('left', parseInt($d.css('left')) - 5);
+        $esp.css('top', parseInt($d.css('top')) - 5);
+    }
 }
 function borrarTodo() {
     $d.removeClass('arriba');
